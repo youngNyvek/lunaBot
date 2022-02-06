974f5f8e3ec8b9953425226d95dfea1e118a5afe
Kevin Faria <kevintxd73@gmail.com>
2022-02-06 03:14:13 -0300

    making continue click

--- LunaBot.py
+++ LunaBot.py
@@ -5,8 +5,18 @@ def moveMouseTo(imageFound):
     imgX, imgY = pyautogui.center(imageFound)
     pyautogui.moveTo(imgX, imgY, 2)
     pyautogui.click()
+    time.sleep(5)
+
+
+def resetWarriors():
+    findingAreAdded = pyautogui.locateAllOnScreen('./targets/areAdded.png', confidence=0.9)
+    for areAdded in findingAreAdded :
+        moveMouseTo(areAdded)
+        time.sleep(5)
+
 
 def huntAreStarted():
+    time.sleep(10)
     victory = pyautogui.locateOnScreen('./targets/victory.png', confidence=0.9)
     defeat = pyautogui.locateOnScreen('./targets/defeat.png', confidence=0.9)
     if victory :
@@ -15,34 +25,43 @@ def huntAreStarted():
         if findingOpenChest :
             moveMouseTo(findingOpenChest)
             findingContinue = pyautogui.locateOnScreen('./targets/tapToContinue.png', confidence=0.9)
-            moveMouseTo(findingContinue)
-            return True
+            if findingContinue :
+                moveMouseTo(findingContinue)
+                return True
 
     if defeat :
         print('Derrota!')
         findingContinue = pyautogui.locateOnScreen('./targets/tapToContinue2.png', confidence=0.9)
-        moveMouseTo(findingContinue)
-        return True
+        if findingContinue :
+            moveMouseTo(findingContinue)
+            return True
 
+def maxWarriorsToBeAdded(num):
+    if num < 3:
+        return num 
+    return 3
 
 def main():
 
    while True :
-    findingEnergy = pyautogui.locateOnScreen('./targets/energy.png', confidence=0.9)
+    resetWarriors()
+    findingEnergy = list(pyautogui.locateAllOnScreen('./targets/energy.png', confidence=0.9))
     if findingEnergy :
-        print('Guerreiros descansados, para a luta!')
+        for i in range(0, maxWarriorsToBeAdded(len(findingEnergy))):
+            moveMouseTo(findingEnergy[i])
+        print('To the battle!')
         findingHuntButton = pyautogui.locateOnScreen('./targets/bosshunt.png', confidence=0.9)
         if findingHuntButton:
             moveMouseTo(findingHuntButton)
             inHunt = True
-            print('Batalha Iniciada!')
+            print('Battle started!')
             time.sleep(15)
             pyautogui.click()
             while inHunt :
                 if huntAreStarted() :
                     inHunt = False
     else : 
-        print('Guerreiros ainda não descansaram')
+        print('No warrior with energy')
         time.sleep(15)
 
 
