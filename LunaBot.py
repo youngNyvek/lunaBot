ca9ef9534fcdf19f81b1374df35428c46c0fc6e9
Kevin Faria <kevintxd73@gmail.com>
2022-02-06 23:01:16 -0300

    adjusting log

--- LunaBot.py
+++ LunaBot.py
@@ -1,26 +1,73 @@
+import os
 import pyautogui
 import time
+from rich.console import Console
+
+console = Console()
+
+victoriesCount = 0
+defeatsCount = 0
 
 def moveMouseTo(imageFound):
     imgX, imgY = pyautogui.center(imageFound)
-    pyautogui.moveTo(imgX, imgY, 2)
+    pyautogui.moveTo(imgX, imgY, 1)
     pyautogui.click()
     time.sleep(5)
 
+def clear():
+    os.system( 'cls' )
+
+def consoleLog(message):
+    clear()
+    mainPrint = """  
+    ________________________________________________________________
+    |            hope i helped, please make a donation              |
+    |                                                               |
+    |                           PayPal                              |
+    |     https://www.paypal.com/donate/?business=DCT8ZXRZYRJM4     |                                
+    |                                                               |
+    |                      BUSD - LUS - BNB                         |
+    |           0x1128731221df2C7BF61736068004550C87B2880A          |                                
+    |_______________________________________________________________|
+     ________________________________________________________________
+    |                                                               |                                
+    |          In case the bot reading is wrong                     |
+    |          try to change some image in the target folder        |                                
+    |_______________________________________________________________|                                                              
+    
+    Infos (obtained since bot startup)      
+                                                        
+    🎉 Victories = {}                     
+                                                        
+    🧨 Defeats =  {}                         
+       
+    """.format(victoriesCount, defeatsCount, message)
+
+    console.print(mainPrint)
+    
+    console.log(f"[blue]{message}")
 
 def resetWarriors():
+    consoleLog("Unchecking warriors for error prevention")
     findingAreAdded = pyautogui.locateAllOnScreen('./targets/areAdded.png', confidence=0.9)
     for areAdded in findingAreAdded :
         moveMouseTo(areAdded)
         time.sleep(5)
 
+def maxWarriorsToBeAdded(num):
+    if num < 3:
+        return num 
+    return 3
 
 def huntAreStarted():
+    global victoriesCount
+    global defeatsCount
     time.sleep(10)
     victory = pyautogui.locateOnScreen('./targets/victory.png', confidence=0.9)
     defeat = pyautogui.locateOnScreen('./targets/defeat.png', confidence=0.9)
     if victory :
-        print('Vitória!')
+        victoriesCount += 1
+        consoleLog('Victory!')
         findingOpenChest = pyautogui.locateOnScreen('./targets/openChest.png', confidence=0.9)
         if findingOpenChest :
             moveMouseTo(findingOpenChest)
@@ -30,42 +77,41 @@ def huntAreStarted():
                 return True
 
     if defeat :
-        print('Derrota!')
+        defeatsCount += 1
+        consoleLog('Defeat!')
         findingContinue = pyautogui.locateOnScreen('./targets/tapToContinue2.png', confidence=0.9)
         if findingContinue :
             moveMouseTo(findingContinue)
             return True
 
-def maxWarriorsToBeAdded(num):
-    if num < 3:
-        return num 
-    return 3
 
 def main():
-
-   while True :
-    resetWarriors()
-    findingEnergy = list(pyautogui.locateAllOnScreen('./targets/energy.png', confidence=0.9))
-    if findingEnergy :
-        for i in range(0, maxWarriorsToBeAdded(len(findingEnergy))):
-            moveMouseTo(findingEnergy[i])
-        print('To the battle!')
-        findingHuntButton = pyautogui.locateOnScreen('./targets/bosshunt.png', confidence=0.9)
-        if findingHuntButton:
-            moveMouseTo(findingHuntButton)
-            inHunt = True
-            print('Battle started!')
-            time.sleep(15)
-            pyautogui.click()
-            while inHunt :
-                if huntAreStarted() :
-                    inHunt = False
-    else : 
-        print('No warrior with energy')
-        time.sleep(15)
+    with console.status("[bold green]LunaBot is running...") as status:
+        while True :
+            resetWarriors()
+            findingEnergy = list(pyautogui.locateAllOnScreen('./targets/energy.png', confidence=0.9))
+            if findingEnergy :
+                consoleLog('Selecting warriors!')
+                for i in range(0, maxWarriorsToBeAdded(len(findingEnergy))):
+                    moveMouseTo(findingEnergy[i])
+                consoleLog('To the battle!')
+                findingHuntButton = pyautogui.locateOnScreen('./targets/bosshunt.png', confidence=0.9)
+                if findingHuntButton:
+                    moveMouseTo(findingHuntButton)
+                    inHunt = True
+                    consoleLog('Battle started!')
+                    time.sleep(15)
+                    pyautogui.click()
+                    while inHunt :
+                        if huntAreStarted() :
+                            inHunt = False
+            else : 
+                consoleLog('No warriors with energy were found.')
+                time.sleep(15)
 
 
 if __name__ == "__main__":
+
     main()
 
     
